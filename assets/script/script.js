22ef2fbbce6853194df2e4698db02a48c5a27819
EdisonR2024 <164270204+EdisonR2024@users.noreply.github.com>
2024-11-11 20:32:49 -0500

    Add files via upload
    Se incluye el archivo script para dar la función de desplegar el menu

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+let menu_nav = document.querySelector("#navbar");
+let boton_menu = menu_nav.querySelector("button");
+
+boton_menu.addEventListener('click', addRemoveMenu);
+
+menu_nav.addEventListener('click', event=>{
+    let elemento_nav = event.target;
+    //Para que se aplique el evento click en todos los links de opciones del menu
+    if (elemento_nav.classList.contains('opcion-menu')) {
+        addRemoveMenu();
+    }
+});
+
+function addRemoveMenu() {
+    menu_nav.classList.toggle('open');
+}
